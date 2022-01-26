7a917b44fbc7f10d7e195835f2654b4f66d61094
laric526 <90987401+laric526@users.noreply.github.com>
2022-01-26 10:27:51 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const post = urlParams.get("post");
+fetchAsync('https://script.google.com/macros/s/AKfycbxrG9dP1KdDUrY3Q3_a4CxRCZ9Cc8TkFONwRkAmbSKexWPU0fONUC6haYjUlY6DlRzw7Q/exec?action=getrecentposts').then(data => {
+  data.result.forEach((element, index) => {
+  	var clone = document.getElementById('post-template').content.cloneNode(true);
+    var post = clone.querySelector('.post');
+    post.href = 'post.html?post=' + element.id;
+    post.querySelector('.post-title').innerHTML = element.title;
+    post.querySelector('.post-metadata').innerHTML = 'id: ' + element.id + ' | time: ' + element.time + ' (' + timeConverter(element.time) + ')';
+    document.getElementById('posts').appendChild(post);
+  });
+});
+
+async function fetchAsync (url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
+
+function timeConverter(timestamp) {
+  var date = new Date(timestamp * 1000);
+  var time = date.toString().split(' ').slice(0, -4).join(' ');
+  return time;
+}
